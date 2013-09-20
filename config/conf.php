49c19df9b242f2e506b4c5bcf453c8383061f8b3
Mr.zogs <guichardsim@gmail@gmail.com>
2013-09-20 10:42:43 +0200

    db

--- config/conf.php
+++ config/conf.php
@@ -95,11 +95,11 @@ class Conf {
     public static function getTransportSwiftMailer(){
 
     	//SMTP configuration
-    	$transport = Swift_SmtpTransport::newInstance()
-    		->setHost('smtp.u-bourgogne.fr')
-    		->setPort(25)
-    		->setUsername('si3804gu')
-    		->setPassword('f4tb0ysl/m');
+    	//$transport = Swift_SmtpTransport::newInstance()
+    	//	->setHost('smtp.u-bourgogne.fr')
+    	//	->setPort(25)
+    	//	->setUsername('si3804gu')
+    	//	->setPassword('f4tb0ysl/m');
 
     		
 	$transport = Swift_SmtpTransport::newInstance('mail.gandi.net',465,"ssl")
