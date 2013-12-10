e99c38b98ffbca623549a3350e9e5f4974b3cf96
Mr.zogs <guichardsim@gmail@gmail.com>
2013-12-10 11:40:11 +0100

    15mo max upload

--- model/articlesModel.php
+++ model/articlesModel.php
@@ -30,7 +30,7 @@ class ArticlesModel extends Model {
 						'destination'=>'media/deposed',
 						'extentions'=>array('doc'),
 						'extentions_error'=>'Your document is not a .doc file',
-						'max_size'=>500000,
+						'max_size'=>15000000,
 						'max_size_error'=>'Your document is too big',
 						'ban_php_code'=>true
 						),
