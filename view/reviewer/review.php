ee65a75dd8d0557d863b9470627102623cb4fbeb
Mr.zogs <guichardsim@gmail@gmail.com>
2014-02-21 12:39:08 +0100

    re

--- view/reviewer/review.php
+++ view/reviewer/review.php
@@ -55,7 +55,7 @@
 	
 	<p style="text-align:center">
 		<a href="<?php echo Router::webroot($$type->filepath);?>">
-			<img src="<?php echo Router::webroot('img/icon-ms-word.png');?>" alt="">
+			<img src="<?php echo Router::webroot('img/icon-ms-word.png');?>" alt="" height="200px">
 			<br>Télécharger le document
 		</a>
 	</p>
@@ -88,7 +88,7 @@
 		Entrer ici votre décision ( accepté ou refusé ). ( Non-communiqué à l'auteur )
 	</p>
 	<?php echo $this->Form->input('prefer','hidden',array('value'=>'poster')); // default value for prefer ?>
-	<?php echo $this->Form->radio('prefer','Communication recommandé',array("class"=>"hide",'poster'=>'Poster','oral'=>"Orale"),array('default'=>$$type->getCommPreferedByReviewer(Session::user()->getID())) );?>
+	<?php echo $this->Form->radio('prefer','Communication recommandé',array('poster'=>'Poster','oral'=>"Orale"),array("class"=>"hide",'default'=>$$type->getCommPreferedByReviewer(Session::user()->getID())) );?>
 
 	<?php echo $this->Form->SelectNumber('note','Note',5,0,array("class"=>"hide",'default'=>$$type->getNoteByReviewer(Session::user()->getID()))); ?>
 
